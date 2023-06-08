9b75e030ccc49f8dbda93ad7a45b4f1a7cfb87a8
dugustlr@naver.com <dugustlr@naver.com>
2023-06-08 17:18:30 +0900

    메인 페이지 디자인

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import Image from "next/image";
-
 export default function Home() {
+  const author = "wilddev_seok";
+  const link = "https://github.com/yeo-hyeonseok";
+
   return (
-    <main>
-      <h1>안녕하세요 케케케</h1>
-    </main>
+    <div className="flex items-center justify-center h-screen flex-col">
+      <h1 className="text-stone-100 text-5xl font-bold text-center">NEFRESH</h1>
+      <a href={link} className="text-stone-100 mt-1">
+        by {author}
+      </a>
+    </div>
   );
 }
