268de3e7481f8ae37b37c94e65aa22cced55124b
GeniuStone <dugustlr@naver.com>
2023-06-12 16:20:29 +0900

    [update] superButton에 className props 추가

--- app/src/components/common/superButton.tsx
+++ app/src/components/common/superButton.tsx
@@ -1,14 +1,19 @@
 "use client";
 
 interface SuperButtonProps {
+  className?: string;
   text: string;
   onClick: VoidFunction;
 }
 
-export default function SuperButton({ text, onClick }: SuperButtonProps) {
+export default function SuperButton({
+  className,
+  text,
+  onClick,
+}: SuperButtonProps) {
   return (
     <button
-      className="text-lg font-semibold bg-orange-400 text-white pt-1 pb-1 pl-2 pr-2 rounded"
+      className={`text-lg font-semibold bg-orange-400 text-white pt-1 pb-1 pl-2 pr-2 rounded ${className}`}
       onClick={onClick}
     >
       {text}
