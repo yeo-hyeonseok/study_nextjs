9b8fa465dd45a5608d9dabe4c7b46bd7058b843d
GeniuStone <dugustlr@naver.com>
2023-06-15 16:48:00 +0900

    [update] mongodb 세팅

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,14 @@
-export default function Home() {
+import { connectDB } from "@/util/db";
+
+export default async function Home() {
   const author = "wilddev_seok";
   const link = "https://github.com/yeo-hyeonseok";
+
+  // 아래와 같은 민감한 코드들은 서버 컴포넌트에서만 사용하는게 좋음
+  const db = (await connectDB).db("forum");
+  const posts = await db.collection("post").find().toArray();
+
+  console.log(posts);
 
   return (
     <div className="flex items-center justify-center h-screen flex-col">
@@ -8,6 +16,7 @@ export default function Home() {
       <a href={link} className="text-stone-100 mt-1">
         by {author}
       </a>
+      <div></div>
     </div>
   );
 }
