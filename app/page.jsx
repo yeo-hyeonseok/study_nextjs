dbd20049c3d1353bf49db1922a7783b0e5c974a4
wilddev_seok <95332775+yeo-hyeonseok@users.noreply.github.com>
2023-06-16 13:45:39 +0900

    Develope (#2)
    * [update] mongodb 세팅
    
    * [delete] 필요없는 코드 및 파일 삭제
    
    * [update] mongodb 데이터 조회

---
+++ app/page.jsx
@@ -0,0 +1,24 @@
+import { connectDB } from "@/util/db";
+
+export default async function Home() {
+  // 아래와 같은 민감한 코드들은 서버 컴포넌트에서만 사용하는게 좋음
+  const db = (await connectDB).db("forum");
+  const posts = await db.collection("post").find().toArray();
+
+  return (
+    <div className="flex items-center h-screen flex-col">
+      <div className="w-1/3">
+        {posts.map((post, index) => (
+          <div key={post._id} className="mt-6">
+            <div className="text-stone-100 text-xl font-semibold border border-slate-100 p-2">
+              TITLE: {post.title}
+            </div>
+            <p className="text-stone-100 text-lg border border-slate-100 p-2">
+              {post.content}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
