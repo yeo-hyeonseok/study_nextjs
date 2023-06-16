68a3afd1ef5d9efd83e22cd766136dd6dbcee64e
GeniuStone <dugustlr@naver.com>
2023-06-16 13:45:04 +0900

    [update] mongodb 데이터 조회

--- app/page.jsx
+++ app/page.jsx
@@ -1,22 +1,24 @@
 import { connectDB } from "@/util/db";
 
 export default async function Home() {
-  const author = "wilddev_seok";
-  const link = "https://github.com/yeo-hyeonseok";
-
   // 아래와 같은 민감한 코드들은 서버 컴포넌트에서만 사용하는게 좋음
   const db = (await connectDB).db("forum");
   const posts = await db.collection("post").find().toArray();
 
-  console.log(posts);
-
   return (
-    <div className="flex items-center justify-center h-screen flex-col">
-      <h1 className="text-stone-100 text-5xl font-bold">NEFRESH</h1>
-      <a href={link} className="text-stone-100 mt-1">
-        by {author}
-      </a>
-      <div></div>
+    <div className="flex items-center h-screen flex-col">
+      <div className="w-1/3">
+        {posts.map((post, index) => (
+          <div key={post._id} className="mt-6">
+            <div className="text-stone-100 text-xl font-semibold border border-slate-100 p-2">
+              TITLE: {post.title}
+            </div>
+            <p className="text-stone-100 text-lg border border-slate-100 p-2">
+              {post.content}
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
