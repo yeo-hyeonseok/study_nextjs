192545d32006744926ff54519abb322ca18d9659
wilddev_seok <95332775+yeo-hyeonseok@users.noreply.github.com>
2023-06-18 21:18:28 +0900

    [update] 상세 페이지 라우팅 기능 추가 (#5)
    * [update] mongodb 세팅
    
    * [delete] 필요없는 코드 및 파일 삭제
    
    * [update] mongodb 데이터 조회
    
    * [update] 내비게이션 바 이름 변경
    
    * [update] 메인화면 ui 변경
    
    - 배경화면 추가
    - 첫 게시글 상단 margin 추가
    - 글로벌 css 추가
    
    * [update] 상세 페이지 라우팅 기능 추가 및 ui 변경
    
    - 게시글 상세 페이지 이동 기능 추가
    - slate 계열 색상 대신 stone-100 색상으로 통일

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,5 @@
 import { connectDB } from "@/util/db";
+import Link from "next/link";
 
 export default async function Home() {
   // 아래와 같은 민감한 코드들은 서버 컴포넌트에서만 사용하는게 좋음
@@ -6,14 +7,17 @@ export default async function Home() {
   const posts = await db.collection("post").find().toArray();
 
   return (
-    <div className="flex items-center h-screen flex-col">
+    <div className="flex items-center flex-col">
       <div className="w-1/3 mt-5">
         {posts.map((post, index) => (
           <div key={post._id} className="mt-6">
-            <div className="text-stone-100 text-xl font-semibold border border-slate-100 p-2">
+            <Link
+              href={"/detail/" + post._id}
+              className="text-stone-100 text-xl font-semibold border border-stone-100 p-2 block"
+            >
               TITLE: {post.title}
-            </div>
-            <p className="text-stone-100 text-lg border border-slate-100 p-2">
+            </Link>
+            <p className="text-stone-100 text-lg border border-stone-100 p-2">
               {post.content}
             </p>
           </div>
