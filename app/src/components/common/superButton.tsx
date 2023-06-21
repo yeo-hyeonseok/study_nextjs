493c78842b3c8b1e6417b365ef98765f425d3632
wilddev_seok <95332775+yeo-hyeonseok@users.noreply.github.com>
2023-06-21 19:15:30 +0900

    [udpate] APIS 페이지 추가 (#7)
    * [update] 내비게이션바 컴포넌트로 분리
    
    * [update] apis 페이지 추가

--- app/src/components/common/superButton.tsx
+++ app/src/components/common/superButton.tsx
@@ -2,21 +2,21 @@
 
 interface SuperButtonProps {
   className?: string;
+  type?: "button" | "submit" | "reset";
   text: string;
-  onClick: VoidFunction;
+  onClick?: VoidFunction;
 }
 
 export default function SuperButton({
   className,
+  type,
   text,
   onClick,
 }: SuperButtonProps) {
   return (
     <button
-      className={
-        "text-lg font-semibold text-white pt-1 pb-1 pl-2 pr-2 border-2 border-stone-100" +
-        className
-      }
+      className={`text-md font-semibold text-white pt-1 pb-1 pl-2 pr-2 border-2 border-stone-100 ${className}`}
+      type={type ?? "button"}
       onClick={onClick}
     >
       {text}
---
+++ pages/api/posts.js
@@ -0,0 +1,32 @@
+import { connectDB } from "@/util/db";
+
+export default async function handler(req, res) {
+  const db = (await connectDB).db("forum");
+
+  // 게시글 조회
+  if (req.method === "GET") {
+    try {
+      const posts = await db.collection("post").find().toArray();
+
+      return res.status(200).json(posts);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  // 게시글 작성
+  if (req.method === "POST") {
+    if (req.body.title === "" || req.body.content == "") {
+      return res.status(500);
+    }
+
+    try {
+      const collection = await db.collection("post");
+      collection.insertOne(req.body);
+
+      return res.redirect(302, "/");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
