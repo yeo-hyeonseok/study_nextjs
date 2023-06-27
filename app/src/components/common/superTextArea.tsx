76d1a1e421c6eed06dedae46b9a3775d3ae11b8b
GeniuStone <dugustlr@naver.com>
2023-06-27 21:39:28 +0900

    [new] superTextArea 컴포넌트 추가

---
+++ app/src/components/common/superTextArea.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { ChangeEvent, useState } from "react";
+
+interface SuperTextAreaProps {
+  id?: string;
+  name?: string;
+  rows?: number;
+  cols?: number;
+  className?: string;
+  value?: string;
+  required?: boolean;
+  readonly?: boolean;
+  disabled?: boolean;
+}
+
+export default function SuperTextArea({
+  id,
+  name,
+  rows,
+  cols,
+  className,
+  value,
+  required,
+  readonly,
+  disabled,
+}: SuperTextAreaProps) {
+  const [textInput, setTextInput] = useState(value);
+
+  const onChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setTextInput(e.target.value);
+  };
+
+  return (
+    <textarea
+      id={id}
+      name={name}
+      rows={rows}
+      cols={cols}
+      className={`w-full pl-1 pr-1 ${className}`}
+      value={textInput}
+      required={required}
+      readOnly={readonly}
+      disabled={disabled}
+      onChange={onChange}
+    />
+  );
+}
