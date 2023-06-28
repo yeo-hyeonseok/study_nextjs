f8a8c6512d692d4f396b35db5e13a1e3a27d241c
GeniuStone <dugustlr@naver.com>
2023-06-28 21:15:42 +0900

    [update] 게시글 수정 기능 추가

--- app/post/[id]/page.jsx
+++ app/post/[id]/page.jsx
@@ -1,6 +1,8 @@
 import { connectDB } from "@/util/db";
 import { ObjectId } from "mongodb";
 import SuperButton from "@/app/src/components/common/superButton";
+import SuperTextField from "@/app/src/components/common/superTextField";
+import SuperTextArea from "@/app/src/components/common/superTextArea";
 
 export default async function Detail(props) {
   const db = (await connectDB).db("forum");
@@ -11,28 +13,22 @@ export default async function Detail(props) {
   return (
     <div className="flex justify-center">
       <form
-        action="/api/posts"
-        method="PATCH"
+        action={`/api/post/updatePost/?id=${post._id.toString()}`}
+        method="POST"
         className="border-2 border-stone-100 p-2 mt-5 w-1/4"
       >
         <div className="flex">
           <label className="text-stone-100 mr-2">TITLE:</label>
-          <input
-            name="title"
-            type="text"
-            className="w-full pl-1 pr-1"
-            value={post.title}
-            required
-          />
+          <SuperTextField name="title" value={post.title} required={true} />
         </div>
         <div className="mt-3">
           <label className="text-stone-100">CONTENT</label>
-          <textarea
+          <SuperTextArea
             name="content"
             rows={3}
-            className="w-full mt-1 pl-1 pr-1"
+            className="mt-1"
             value={post.content}
-            required
+            required={true}
           />
         </div>
         <div className="flex justify-end mt-3">
---
+++ pages/api/post/updatePost.js
@@ -0,0 +1,33 @@
+import { connectDB } from "@/util/db";
+import { ObjectId } from "mongodb";
+
+export default async function handler(req, res) {
+  const db = (await connectDB).db("forum");
+
+  // 게시글 수정
+  if (req.method === "POST") {
+    if (req.body.title === "" || req.body.content == "") {
+      return res.status(500).json("제목이나 내용 입력 해주셈");
+    }
+
+    try {
+      const collection = await db.collection("post");
+
+      collection.updateOne(
+        {
+          _id: new ObjectId(req.query.id),
+        },
+        {
+          $set: {
+            title: req.body.title,
+            content: req.body.content,
+          },
+        }
+      );
+
+      return res.redirect(302, "/");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
