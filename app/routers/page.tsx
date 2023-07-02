3ecf05f65942d2264c431d27154c80627af14b96
GeniuStone <dugustlr@naver.com>
2023-07-02 17:36:06 +0900

    [update] routerItem 컴포넌트 이름 및 위치 변경
    - 파일명 routerItem => superCardItem으로 변경
    - 기존 components/routers 폴더에서 components/common 폴더로 위치 변경

--- app/routers/page.tsx
+++ app/routers/page.tsx
@@ -5,7 +5,7 @@ import {
   useRouter,
   useSearchParams,
 } from "next/navigation";
-import RouterItem from "../src/components/routers/routerItem";
+import SuperItemCard from "../src/components/common/superItemCard";
 
 export default function Routers() {
   let router = useRouter();
@@ -16,68 +16,68 @@ export default function Routers() {
   return (
     <div className="flex items-center flex-col pt-4 pb-8">
       <div className="w-1/3">
-        <RouterItem
+        <SuperItemCard
           className="mt-4"
           label="router.push('/')"
-          onClick={() => {
+          isButton={true}
+          onClickButton={() => {
             router.push("/");
           }}
           desc="특정 페이지로 이동할 때 사용하셈"
         />
-        <RouterItem
+        <SuperItemCard
           className="mt-6"
           label="router.back()"
-          onClick={() => {
+          isButton={true}
+          onClickButton={() => {
             router.back();
           }}
           desc="뒤로가기"
         />
-        <RouterItem
+        <SuperItemCard
           className="mt-6"
           label="router.forward()"
-          onClick={() => {
+          isButton={true}
+          onClickButton={() => {
             router.forward();
           }}
           desc="앞으로가기"
         />
-        <RouterItem
+        <SuperItemCard
           className="mt-6"
           label="router.refresh()"
-          onClick={() => {
+          isButton={true}
+          onClickButton={() => {
             router.refresh();
           }}
           desc="새로고침인데 페이지 전체를 새로 로드하는 건 아니고 변경이 필요한 부분만 재렌더링 해주는 거임. Soft refresh"
         />
-        <RouterItem
+        <SuperItemCard
           className="mt-6"
           label="router.prefetch('/products')"
-          onClick={() => {
+          isButton={true}
+          onClickButton={() => {
             router.prefetch("/products");
           }}
           desc="해당 url의 페이지의 내용을 미리 로드해줌. 개발 환경에서는 기능 동작 관측 불가. Link 컴포넌트에는 기본적으로 내장되어 있는 기능임. (다만, Link가 너무 많거나 페이지의 내용을 미리 로드할 필요가 없을 때는 prefetch={false}로 꺼두는게 좋을듯)"
         />
-        <RouterItem
+        <SuperItemCard
           className="mt-6"
           label="usePathname()"
-          onClick={() => {
+          isButton={true}
+          onClickButton={() => {
             alert(pathName);
           }}
           desc="현재 url 반환해줌"
         />
-        <RouterItem
+        <SuperItemCard
           className="mt-6"
           label="useParams()"
-          onClick={() => {
-            console.log(params);
-          }}
           desc="다이내믹 라우트에 적용된 url 파라미터 반환해줌"
         />
-        <RouterItem
+        <SuperItemCard
           className="mt-6"
           label="useSearchParams()"
-          onClick={() => {
-            alert(searchParams);
-          }}
           desc="query string 반환해줌"
         />
       </div>
--- app/src/components/common/superItemCard.tsx
+++ app/src/components/common/superItemCard.tsx
@@ -1,25 +1,27 @@
 import SuperButton from "../common/superButton";
 
-interface RouterItemProps {
+interface SuperItemCardProps {
   className?: string;
   label: string;
-  onClick: VoidFunction;
   desc: string;
+  isButton?: boolean;
+  onClickButton?: VoidFunction;
 }
 
-export default function RouterItem({
+export default function SuperItemCard({
   className,
   label,
-  onClick,
   desc,
-}: RouterItemProps) {
+  isButton,
+  onClickButton,
+}: SuperItemCardProps) {
   return (
     <div>
       <div
         className={`flex border border-stone-100 p-2 justify-between items-center ${className}`}
       >
         <h2 className="text-stone-100 text-xl font-semibold">{label}</h2>
-        <SuperButton text="눌러보셈" onClick={onClick} />
+        {isButton && <SuperButton text="눌러보셈" onClick={onClickButton} />}
       </div>
       <p className="text-stone-100 text-lg border border-stone-100 p-2">
         {desc}
