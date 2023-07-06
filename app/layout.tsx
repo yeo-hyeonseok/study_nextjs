caba73b126a7d5259dd7ca863ecfe01e1a542bd0
GeniuStone <dugustlr@naver.com>
2023-07-06 17:53:46 +0900

    [new] 페이지 상단에 유저 정보 영역 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { getServerSession } from "next-auth";
+import Image from "next/image";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import Navigation from "./src/components/common/navigation";
+import LoginButton from "./src/components/common/loginButton";
+import LogoutButton from "./src/components/common/logoutButton";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,14 +14,39 @@ export const metadata = {
   description: "next13 & tailwind & 배포 연습하기",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  let session = await getServerSession(authOptions);
+
+  console.log(session);
+
   return (
     <html lang="en">
       <body className={`${inter.className} bg-goku`}>
+        <div className="p-2 border border-stone-100">
+          {session !== null ? (
+            <div className="flex justify-between">
+              <div className="flex items-center">
+                <Image
+                  src={session?.user?.image ?? ""}
+                  alt="프로필"
+                  width={32}
+                  height={32}
+                  className="mr-2"
+                />
+                <span className="text-stone-100">{session?.user?.name}</span>
+              </div>
+              <LogoutButton className="" />
+            </div>
+          ) : (
+            <div className="flex justify-end">
+              <LoginButton className="" />
+            </div>
+          )}
+        </div>
         <Navigation />
         {children}
       </body>
--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,8 @@
-import LoginButton from "./src/components/main/loginButton";
-
 export default async function Home() {
   return (
     <div className="flex items-center justify-center h-screen flex-col">
       <div className="w-1/6 flex flex-col items-center">
         <h1 className="text-stone-100 text-2xl">안녕하세요 방가방가</h1>
-        <LoginButton className="w-full mt-4" />
       </div>
     </div>
   );
