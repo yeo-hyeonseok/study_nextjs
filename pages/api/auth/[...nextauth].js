d97b6b4084230dbd704d375c5d38fcf22f688418
GeniuStone <dugustlr@naver.com>
2023-07-09 16:21:32 +0900

    [update] Credential 로그인 기능 추가

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,14 +1,79 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { connectDB } from "@/util/db";
+//import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
 
 export const authOptions = {
   providers: [
     GithubProvider({
-      clientId: "",
-      clientSecret: "",
+      clientId: "b2fb2ddb9211af8c40c7",
+      clientSecret: "f9eb3d3748ab9cc67be2f2411efef2e73c394c0f",
+    }),
+    CredentialsProvider({
+      //1. 로그인페이지 폼 자동생성해주는 코드
+      name: "credentials",
+      credentials: {
+        email: { label: "email", type: "text" },
+        password: { label: "password", type: "password" },
+      },
+
+      //2. 로그인요청시 실행되는코드
+      //직접 DB에서 아이디, 비번 비교하고
+      //아이디,비번 맞으면 return 결과, 틀리면 return null 해야함
+      async authorize(credentials) {
+        let db = (await connectDB).db("forum");
+        let user = await db
+          .collection("user")
+          .findOne({ email: credentials.email });
+        if (!user) {
+          console.log("해당 이메일은 없음");
+          return null;
+        }
+        const pwcheck = await bcrypt.compare(
+          credentials.password,
+          user.password
+        );
+        if (!pwcheck) {
+          console.log("비번틀림");
+          return null;
+        }
+        return user;
+      },
     }),
   ],
-  secret: "JWT 발금 시 사용할 암호",
+
+  //3. jwt 써놔야 잘됩니다 + jwt 만료일설정
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60, //30일
+  },
+
+  callbacks: {
+    //4. jwt 만들 때 실행되는 코드
+    //jwt에 기입할 정보임
+    // 유저 역할도 부여할 수 있음
+    //user변수는 DB의 유저정보담겨있고 token.user에 뭐 저장하면 jwt에 들어갑니다.
+    jwt: async ({ token, user }) => {
+      if (user) {
+        token.user = {};
+        token.user.image =
+          user.image ?? "https://avatars.githubusercontent.com/u/95332775?v=4";
+        token.user.name = user.username ?? user.name;
+        token.user.email = user.email;
+      }
+      return token;
+    },
+    //5. 유저 세션이 조회될 때 마다 실행되는 코드
+    session: async ({ session, token }) => {
+      session.user = token.user;
+      return session;
+    },
+  },
+
+  //adapter: MongoDBAdapter(connectDB),
+  secret: "qwer1234",
 };
 
 export default NextAuth(authOptions);
