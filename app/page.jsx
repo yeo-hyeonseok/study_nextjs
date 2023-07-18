16a0dc08ca988bf9556a5cd48d2f9013818a91ec
GeniuStone <dugustlr@naver.com>
2023-07-18 18:41:02 +0900

    [update] 메인 페이지 필요없는 코드 삭제

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,7 @@
 export default function Home() {
   return (
     <div className="flex items-center justify-center h-screen flex-col">
-      <div className="w-1/6 flex flex-col items-center">
-        <h1 className="text-stone-100 text-2xl">안녕하세요 방가방가</h1>
-      </div>
+      <h1 className="text-stone-100 text-2xl">안녕하세요 방가방가</h1>
     </div>
   );
 }
