64dc59cf49532679e5359467141d9218e73b6573
GeniuStone <dugustlr@naver.com>
2023-07-19 15:55:39 +0900

    [new] 커스텀 404 페이지 추가

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+// 커스텀 404 페이지
+
+export default function NotFound() {
+  return (
+    <div className="flex items-center justify-center h-screen flex-col">
+      <h1 className="text-stone-100 text-2xl">404 없는 페이지임</h1>
+    </div>
+  );
+}
