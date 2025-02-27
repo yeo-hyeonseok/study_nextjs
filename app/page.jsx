39cf1917ba7a6b0d260f2c03b11233c72acf6e19
dugustlr@naver.com <dugustlr@naver.com>
2025-02-27 15:50:03 +0900

    [DELETE] 프로젝트 초기화
    - next15 버전 공부를 위한 프로젝트 초기화
    - 기존 내용은 'next13' 브랜치로 분기함

--- app/page.jsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex items-center justify-center h-screen flex-col">
-      <h1 className="text-stone-100 text-2xl">안녕하세요 방가방가</h1>
-    </div>
-  );
-}
