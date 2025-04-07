c219c1c22f8dc22537fe48c4c6f143dda5c308ab
dugustlr@naver.com <dugustlr@naver.com>
2025-04-07 21:10:22 +0900

    [UPDATE] auth.js 기본 세팅

---
+++ auth.ts
@@ -0,0 +1,16 @@
+import NextAuth from "next-auth";
+
+// handlers: '/api/auth/[...nextauth]/route.ts'에 쓰이는 라우트 핸들러
+// signIn: 클라이언트에서 로그인 시 사용하는 함수
+// signOut: 클라이언트에서 로그아웃 시 사용하는 함수
+// auth: 서버 컴포넌트나 미들웨어에서 세션 정보를 가져오는 함수
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [],
+  // callbacks 속성을 통해서 인증 흐름을 커스터마이징할 수 있음
+  callbacks: {
+    // 인증된 사용자인지 아닌지 판별
+    authorized: async ({ auth }) => {
+      return !!auth;
+    },
+  },
+});
--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,10 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { getRandomNum } from "./lib/utils";
+import { auth } from "./auth";
 
-// 경로 이동 전에 서버에서 수행할 동작을 작성할 수 있음
-// middleware 함수는 암묵적으로 NextResponse.next()를 반환함
-// NextResponse.next()는 요청을 그대로 통과시켜 원래 요청된 페이지로 진행하도록 하는 메서드임
-export function middleware(request: NextRequest) {
+// auth: auth.js v5에서 미들웨어 구현 시 기본 래퍼로 사용
+export default auth(function middleware(request: NextRequest) {
   if (request.nextUrl.pathname === "/digimons") return NextResponse.next();
 
   const digimonId = Number(
@@ -17,7 +16,7 @@ export function middleware(request: NextRequest) {
       new URL(`/digimons/${getRandomNum(1460)}`, request.url)
     );
   }
-}
+});
 
 // matcher 속성에 지정해둔 경로일 경우에만 미들웨어가 실행됨
 // config 생략 시 모든 경로에서 미들웨어 실행
